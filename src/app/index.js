dbbdaf22173c092608a4b2b4e634b91702aa0953
Andrey Pushkarev <fealaer@gmail.com>
2015-11-23 13:53:52 +0800

    Fixed router - home ctrl and redirection based on user status Added angular-messages to the project

--- src/app/index.js
+++ src/app/index.js
@@ -2,11 +2,24 @@
   'use strict';
 
   angular
-    .module('realine', ['ngRoute', 'realine.public'])
-    .config(['$locationProvider',
-      function ($locationProvider) {
+    .module('realine', ['ngRoute', 'ngMessages', 'realine.public'])
+    .config(['$locationProvider', '$routeProvider',
+      function ($locationProvider, $routeProvider) {
+        $routeProvider
+          .when('/', {
+            controller: 'HomeCtrl'
+          })
+          .otherwise({
+            redirectTo: '/'
+          });
+
         $locationProvider.html5Mode(true);
       }])
-    .controller('MainCtrl', function () {});
+    .controller('HomeCtrl', ['$location', 'UserServ', HomeCtrl]);
+
+  function HomeCtrl($location, UserServ) {
+    console.log(UserServ.isLoggedIn());
+    $location.path(UserServ.isLoggedIn() ? '/login' : '/login');
+  }
 
 })();
