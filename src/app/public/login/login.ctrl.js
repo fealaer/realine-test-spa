1f4c9d585dc8f289d2daa9006ac1345675302704
Andrey Pushkarev <fealaer@gmail.com>
2015-11-23 14:12:52 +0800

    Refactored: controller and login form - show form status not only errors messages - alerts instead of help-notes

--- src/app/public/login/login.ctrl.js
+++ src/app/public/login/login.ctrl.js
@@ -4,27 +4,33 @@
   function LoginCtrl($timeout, UserServ) {
     var vm = this;
     vm.data = {};
-    vm.formError = null;
+    vm.formStatus = null;
 
     vm.login = login;
-    vm.cleanFormError = cleanFormError;
+    vm.setFormError = setFormStatus;
     vm.hasError = hasError;
     vm.getUsers = getUsers;
 
     function login() {
       if (vm.form.$valid) {
         UserServ.login(vm.data).then(function (result) {
-          console.log(result);
+          if (result) {
+            setFormStatus({loggedIn: true});
+
+          }
         }, function (err) {
-          console.log(err);
-          vm.formError = err;
-          $timeout(cleanFormError, 3000);
+          setFormStatus(err);
         });
       }
     }
 
-    function cleanFormError() {
-      vm.formError = null;
+    function setFormStatus(status) {
+      vm.formStatus = status;
+      $timeout(cleanFormStatus, 3000);
+    }
+
+    function cleanFormStatus() {
+      vm.formStatus = null;
     }
 
     function hasError(elem) {
